27d05ebbb19b90bcec9da4ba5f56510fe8eb6135
varungobinath <varung31102004@gmail.com>
2024-02-12 20:34:02 +0530

    adding Pattern_08

---
+++ src/main/java/patterns/Pattern_08.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class Pattern_08 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=n;i>=1;i--){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+//*********
+// *******
+//  *****
+//   ***
+//    *
