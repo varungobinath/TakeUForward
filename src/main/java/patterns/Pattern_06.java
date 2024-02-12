6dfd42bfb9e1b92d7a0e928a5fa4c4b87755b137
varungobinath <varung31102004@gmail.com>
2024-02-12 20:15:01 +0530

    adding Pattern_06

---
+++ src/main/java/patterns/Pattern_06.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class Pattern_06 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+//12345
+//1234
+//123
+//12
+//1
