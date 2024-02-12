9fb6e7e033dbbc6f70b10c814e39d5ca4738159b
varungobinath <varung31102004@gmail.com>
2024-02-12 20:48:14 +0530

    adding Pattern_10

---
+++ src/main/java/patterns/Pattern_10.java
@@ -0,0 +1,28 @@
+package patterns;
+
+public class Pattern_10 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
