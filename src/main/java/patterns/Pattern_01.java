45981d8ea11fcb4a605520924741fd1f1d589396
varungobinath <varung31102004@gmail.com>
2024-02-12 19:36:30 +0530

    1st commmit

--- src/main/java/patterns/Pattern_01.java
+++ src/main/java/patterns/Pattern_01.java
@@ -1,6 +1,6 @@
-package org.example;
+package patterns;
 
-public class Patterns {
+public class Pattern_01 {
     public static void main(String[] args) {
         int n = 5;
         for (int i=0;i<n;i++){
@@ -9,11 +9,10 @@ public class Patterns {
             }
             System.out.println();
         }
-        for (int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
     }
 }
+//*****
+//*****
+//*****
+//*****
+//*****
