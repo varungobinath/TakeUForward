b80d595570a4f249b46a5e9ed54cadcfea6adb29
varungobinath <varung31102004@gmail.com>
2025-06-21 20:21:48 +0530

    added all files

---
+++ src/Recursion/Hard/WordSearch.java
@@ -0,0 +1,4 @@
+package Recursion.Hard;
+
+public class WordSearch {
+}
---
+++ src/Recursion/Medium/SubSetSum.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubSetSum {
+    private static void subsetSum(int[] arr,int ind,int sum,List<Integer> ans){
+        if(ind==arr.length){
+            ans.add(sum);
+            return;
+        }
+        subsetSum(arr,ind+1,sum+arr[ind],ans);
+        subsetSum(arr,ind+1,sum,ans);
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,2,1};
+//        Arrays.sort(arr);
+        List<Integer> ans = new ArrayList<>();
+        subsetSum(arr,0,0,ans);
+        System.out.println(ans);
+    }
+}
